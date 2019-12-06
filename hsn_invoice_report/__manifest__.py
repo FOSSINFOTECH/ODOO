11553ddd8a82e2b28ab24f98aabf25f1400631c2
Shanthi <shanthi@fossinfotech.com>
2019-12-06 13:22:57 +0530

    new module added

---
+++ hsn_invoice_report/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "HSN Invoice Report",
+
+    'summary': """HSN based grouping of Products in Invoice""",
+
+    'description': """
+        This module adds a functionality for HSN based grouping of Products in Invoice.
+    """,
+
+    'license': 'AGPL-3',
+
+    'author': "FOSS INFOTECH PVT LTD",
+
+    'website': "http://www.fossinfotech.com",
+
+    'category': 'Accounting',
+
+    'version': '13.0.0.0.0',
+
+    'depends': ['account', 'l10n_in'],
+
+    'data': [
+        'report/reports.xml',
+        'report/invoice_report_template.xml',
+        'report/layout.xml',
+    ],
+
+    'images': [
+        'static/description/banner.png',
+        'static/description/icon.png',
+        'static/description/index.html',
+    ],
+
+    'installable': True,
+
+    'auto_install': False,
+
+    'application': True,
+
+}
